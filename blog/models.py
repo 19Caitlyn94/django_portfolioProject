e4b261a9b0bce9cbbcc91d38446c92169ee160bc
CaitlynFowkes <caitlynfowkes@gmail.com>
2019-10-09 20:50:06 +0200

    Add label single-select to blog model

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
+    BUSINESS = 'Business'
+    WORK = 'Work'
+    STUDY = 'Study'
+    OTHER = 'Other'
+
+    LABEL_CHOICES = [
+        (BUSINESS, 'Business'),
+        (WORK, 'Work'),
+        (STUDY, 'Study'),
+        (OTHER, 'Other')
+    ]
+
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     body = models.CharField(max_length=200)
     banner = models.ImageField(upload_to='images/')
-    
+    label = models.CharField(
+        max_length=20,
+        choices=LABEL_CHOICES,
+        default=OTHER,
+    ) 
+
+    def __str__(self):
+        return self.title
+
     def summary(self):
         if len(self.body) > 100: 
             return self.body[:100] + "..."
