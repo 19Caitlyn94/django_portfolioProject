febcf2c9b43412aa074bd0048b36394d49a05ab1
CaitlynFowkes <caitlynfowkes@gmail.com>
2019-10-10 16:13:16 +0200

    Access blog detail using the id

--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs),
+    path('', views.allblogs, name='allBlogs'),
+    path('<int:blog_id>/', views.detail, name="detail")
 ]
