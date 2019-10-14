9864ff63985a53495bd4868c159651992455e862
CaitlynFowkes <caitlynfowkes@gmail.com>
2019-10-14 08:07:45 +0200

    Fix url names

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='allBlogs'),
+    path('', views.allblogs, name='allblogs'),
     path('<int:blog_id>/', views.detail, name="detail")
 ]
